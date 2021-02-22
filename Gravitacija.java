d739e082fc34f458596d719f490c0c96efb10650
AnejVovcak <av3363@student.uni-lj.si>
2021-02-22 14:11:51 +0100

    Dodajanje izračuna pospeška

--- Gravitacija.java
+++ Gravitacija.java
@@ -2,4 +2,14 @@ public class Gravitacija {
     public static void main(String[] args) {
         System.out.println("OIS je zakon");
     }
+
+    public static double gravitacijskiPospesek(double visina){
+		
+		double konstC = 6.674*Math.pow(10,-11);
+		double m = 5.972*Math.pow(10,24);
+		double stevec = konstC*m;
+		double polmer = 6.371*Math.pow(10,6);
+		double imenovalec = Math.pow(polmer+visina, 2);
+		return stevec/imenovalec;
+	}
 }
