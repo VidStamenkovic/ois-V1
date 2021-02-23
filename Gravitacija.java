046b74886291561f25ade764d4b9cd32d079fac5
klemenkerin <klemen.kerin01@gmail.com>
2021-02-23 11:45:51 +0100

    Dodajanje funkcije za izpis pospeška in višine

--- Gravitacija.java
+++ Gravitacija.java
@@ -2,4 +2,9 @@ public class Gravitacija {
     public static void main(String[] args) {
         System.out.println("OIS je zakon");
     }
+
+    public static void izpis(double nadmorskaVisina, double gravitacijskiPospesek) {
+        System.out.println("Nadmorska višina:" + nadmorskaVisina);
+        System.out.println("Gravitacijski pospešek:" + gravitacijskiPospesek);
+    }
 }
